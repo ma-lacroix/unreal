063d722e476b88e0ec9239af072d9002e4a38e52
ma-lacroix <lacroix.marcantoine@gmail.com>
2021-07-12 12:49:48 +0200

    minor change

--- triple/main.cpp
+++ triple/main.cpp
@@ -1,6 +1,19 @@
+#include <string>
 #include <iostream>
 
-int main{
+int main(){
+    
     std::cout << "Hello world" << std::endl;
+    
+    char answer;
+
+    while(std::tolower(answer)!='q'){
+        std::cout << "enter a letter: " << std::endl;
+        std::cin >> answer;
+        std::cout << "You entered: \n" << answer << std::endl;
+
+    }
+
+    
     return 0;
 }
